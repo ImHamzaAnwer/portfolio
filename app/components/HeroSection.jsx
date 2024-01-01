1fa4a1852cb6645cb567eb602dd9542da143cf7f
Hamza Anwer <hamza.anwer8@gmail.com>
2024-01-02 02:46:37 +0500

    hide image on small screens

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -61,7 +61,7 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+          <div className="sm:block hidden shadow-lg rounded-full relative  w-[230px] h-[230px] lg:w-[370px] lg:h-[370px]">
             <Image
               src="/images/hero-image.jpeg"
               alt="hero image"
