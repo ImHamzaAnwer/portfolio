83b03e66d9dc9461f9c8366f53a834196cccf736
Hamza Anwer <hamza.anwer8@gmail.com>
2024-01-02 01:37:44 +0500

    added gradient on mouse tracking

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+"use client";
+import { useEffect, useRef } from "react";
 import AboutSection from "./components/AboutSection";
 import AchievementsSection from "./components/AchievementsSection";
 import EmailSection from "./components/EmailSection";
@@ -7,16 +9,46 @@ import Navbar from "./components/Navbar";
 import ProjectsSection from "./components/ProjectsSection";
 
 export default function Home() {
+  const heroRef = useRef(null);
+
+  useEffect(() => {
+    const updateMousePosition = (ev) => {
+      if (!heroRef.current) return;
+      const { clientX, clientY } = ev;
+      heroRef.current.style.setProperty("--x", `${clientX}px`);
+      heroRef.current.style.setProperty("--y", `${clientY}px`);
+    };
+
+    window.addEventListener("mousemove", updateMousePosition);
+
+    return () => {
+      window.removeEventListener("mousemove", updateMousePosition);
+    };
+  }, []);
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]">
-      <div className="container mx-auto px-12 py-4">
-        <HeroSection />
-        <AchievementsSection />
-        <AboutSection />
-        <ProjectsSection />
-        <EmailSection />
-        {/* <Footer /> */}
-      </div>
-    </main>
+    <>
+      <style jsx>{`
+        .custom-gradient-bg {
+          background-image: radial-gradient(
+            600px at var(--x, 100px) var(--y, 100px),
+            rgba(29, 78, 216, 0.15),
+            transparent 80%
+          );
+        }
+      `}</style>
+      <main
+        ref={heroRef}
+        className="custom-gradient-bg flex min-h-screen flex-col bg-slate-900"
+      >
+        <div className="container mx-auto px-12 py-4">
+          <HeroSection />
+          <AchievementsSection />
+          <AboutSection />
+          <ProjectsSection />
+          <EmailSection />
+          {/* <Footer /> */}
+        </div>
+      </main>
+    </>
   );
 }
